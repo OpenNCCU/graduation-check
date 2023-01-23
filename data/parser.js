c2f8eb9927ed3c77581000cd8efcb806325b1b15
rongting <88624943+z15r7@users.noreply.github.com>
2023-01-23 16:54:45 +0800

    Update parser.js

--- data/parser.js
+++ data/parser.js
@@ -4,10 +4,11 @@ import required from './required.js';
 const parse = () => {
   const data = required;
   const result = { ...data };
-  result.subjects = data.children.map((subject) => {
+  result.aaa = data['107'].map(e => e.children.map((subject) => {
     const r = {};
     r.name = subject['科目名稱'];
     r.type = subject['修別'];
+    r.semester = [subject['第一學期上']]
     return r;
   });
   fs.writeFileSync('./data/test.json', JSON.stringify(result, null, 2));
