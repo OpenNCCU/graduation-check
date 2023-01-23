11329b564c5f4b6ad650a610c9e2c254221a5ec0
z15r7 <z15r7x@gmail.com>
2023-01-23 18:35:23 +0800

    update

--- data/parser.js
+++ data/parser.js
@@ -12,7 +12,8 @@ const parse = () => {
         r1.name = subject['科目名稱'];
         r1.type = subject['修別'];
         r1.semester = [subject['第一學年上'], subject['第一學年下'], subject['第二學年上'], subject['第二學年下'], subject['第三學年上'], subject['第三學年下'], subject['第四學年上'], subject['第四學年下']].map((e) => e !== '無');
-        r1.constraint = subject['須修本門課之科目代碼'] === '無' ? (subject['認定方式'] === '不限' ? '' : item.dept.substring(0, (subject['認定方式'] === '需為本系開課' ? 3 : 1))) : subject['須修本門課之科目代碼']
+        // eslint-disable-next-line no-nested-ternary
+        r1.constraint = subject['須修本門課之科目代碼'] === '無' ? (subject['認定方式'] === '不限' ? '' : item.dept.substring(0, (subject['認定方式'] === '需為本系開課' ? 3 : 1))) : subject['須修本門課之科目代碼'];
         r1.group = subject['修別'].includes('群') ? subject['修別'].substring(1, 2) : '';
         return r1;
       });
