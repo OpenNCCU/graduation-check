3f869d40e5d21650c3f6e6f3501cfcf755d78ad2
z15r7 <z15r7x@gmail.com>
2023-01-24 00:08:39 +0800

    update for advance data cleaning

--- data/parser.js
+++ data/parser.js
@@ -1,38 +1,101 @@
 import fs from 'fs';
-import required from './required.js';
+import requiredSource from './required.js';
 
 const parse = () => {
-  const data = required;
-  const result = { ...data };
-  Object.keys(result).forEach((year) => {
-    result[year] = result[year].map((item) => {
-      const r = {};
+  const data = { ...requiredSource };
+  Object.keys(data).forEach((year) => {
+    data[year] = data[year].map((item) => {
+      const require = {
+        type: 'major',
+        deptID: item.dept,
+        group: item.group,
+        year: item.year,
+      };
 
-      r.year = item.year;
-      r.department_name = item.department_name;
-      r.group_name = item.group_name;
-      r.requireCredit = item.requireCredit;
-      r.minTotalCredit = item.group_condition.find((text) => text.includes('本系最低畢業總學分數'));
-      r.spacialty = item.spacialty;
+      // require.departmentName = item.department_name;
+      // require.groupName = item.group_name;
 
-      r.subjects = item.children.map((subject) => ({
-        name: subject['科目名稱'],
-        type: subject['修別'].substring(0, 1),
-        semester: [subject['第一學年上'], subject['第一學年下'], subject['第二學年上'], subject['第二學年下'], subject['第三學年上'], subject['第三學年下'], subject['第四學年上'], subject['第四學年下']].map((e) => e !== '無'),
-        group: subject['修別'].includes('群') ? subject['修別'].substring(1, 2) : '',
-        maxCredit: subject['規定學分'],
-        minCredit: subject['規定學分'],
-        semCount: subject['學期數'],
-        // eslint-disable-next-line no-nested-ternary
-        constraint: subject['須修本門課之科目代碼'] === '無' ? (subject['認定方式'] === '不限' ? '' : item.dept.substring(0, (subject['認定方式'] === '需為本系開課' ? 3 : 1))) : subject['須修本門課之科目代碼'],
-        note: subject['備註(先修科目說明)'],
-      }));
-      return r;
+      require.requireCredit = item.credit;
+      require.minTotalCredit = parseInt(
+        item.group_condition
+          .find((text) => text.includes('本系最低畢業總學分數：'))
+          .replace(/本系最低畢業總學分數：([0-9]+)學分/, '$1'),
+        10,
+      );
+      // require.group_condition = item.group_condition;
+      // require.spacialty = item.spacialty;
+      require.PErequire = item.spacialty.find((text) => text.match(/(體育).*(選修)/));
+      require.NDrequire = item.spacialty.find((text) => text.match(/國防/));
+
+      require.rules = [];
+
+      item.children.forEach((ruleBase) => {
+        let constraint;
+        if (ruleBase['須修本門課之科目代碼'] !== '無') {
+          constraint = ruleBase['須修本門課之科目代碼'];
+        }
+        if (ruleBase['須修本門課之科目代碼'] === '無' && ruleBase['認定方式'] !== '不限') {
+          switch (ruleBase['認定方式']) {
+            case '需為本系開課':
+              constraint = item.dept.substring(0, 3);
+              break;
+            case '需為本院開課':
+              constraint = item.dept.substring(0, 1);
+              break;
+            default:
+              break;
+          }
+        }
+        let group;
+        if (ruleBase['修別'].replace(/必修|群([A-Z])/, '$1').length > 0) {
+          group = ruleBase['修別'].replace(/必修|群([A-Z])/, '$1');
+        }
+        const rule = {
+          group,
+          minCredit: parseInt(ruleBase['規定學分'], 10),
+          maxCredit: parseInt(ruleBase['規定學分'], 10),
+          semesterCount: parseInt(ruleBase['學期數'], 10),
+          courseCount: group ? ruleBase['備註(先修科目說明)'] : 1,
+          note: ruleBase['備註(先修科目說明)'],
+          subjects: [],
+        };
+        const subject = {
+          name: ruleBase['科目名稱'],
+          semester: [ruleBase['第一學年上'], ruleBase['第一學年下'], ruleBase['第二學年上'], ruleBase['第二學年下'], ruleBase['第三學年上'], ruleBase['第三學年下'], ruleBase['第四學年上'], ruleBase['第四學年下']].map((e) => e !== '無'),
+          constraint,
+        };
+        if (!group) {
+          rule.subjects.push(subject);
+          require.rules.push(rule);
+        } else {
+          const lastRule = require.rules[require.rules.length - 1];
+          if (JSON.stringify({
+            group: lastRule.group,
+            // maxCredit: lastRule.maxCredit,
+            // minCredit: lastRule.minCredit,
+            // semCount: lastRule.semCount,
+            // note: lastRule.note,
+          })
+            === JSON.stringify({
+              group: rule.group,
+              // maxCredit: rule.maxCredit,
+              // minCredit: rule.minCredit,
+              // semCount: rule.semCount,
+              // note: rule.note,
+            })) {
+            lastRule.subjects.push(subject);
+            return;
+          }
+          rule.subjects.push(subject);
+          require.rules.push(rule);
+        }
+      });
+      return require;
     });
 
-    fs.writeFileSync('./data/test.json', JSON.stringify(result, null, 2));
+    fs.writeFileSync('./data/test.json.local', JSON.stringify(data, null, 2));
   });
-  return result;
+  return data;
 };
 
 export default parse;
