b363bd447f57c926d87b6ab93f5bc21fcdc2e7bb
rongting <88624943+z15r7@users.noreply.github.com>
2023-01-23 17:41:50 +0800

    Update app.js

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ app.listen(port, () => {
 });
 
 app.get('*', (req, res) => {
-  parser();
+  const data = parser();
   // console.log(req.url)
-  req.body = dummydata;
-  const data = handleData(req.body);
+  // req.body = dummydata;
+  // const data = handleData(req.body);
   const result = { data, success: false, error: 'NULL PATH.' };
   res.send(result);
 });
