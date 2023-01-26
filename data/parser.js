c88ac5cb049b645ce9e60c1bc799dcdaeadc110d
z15r7 <z15r7x@gmail.com>
2023-01-26 15:14:41 +0800

    groupCondition

--- data/parser.js
+++ data/parser.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 const parse = () => {
   fs.writeFileSync('./data/output.json.local', '');
   // fs.writeFileSync('./data/output.csv.local', `"${'result.PErequire'}","${'result.NDrequire'}"\n`);
-  fs.writeFileSync('./data/output.csv.local', '');
+  // fs.writeFileSync('./data/output.csv.local', '');
 
   const data = JSON.parse(fs.readFileSync('./data/result.json'));
 
@@ -11,30 +11,67 @@ const parse = () => {
     data[year] = data[year].map((require) => {
       const result = {};
 
-      result.PErequire = require.spacialty.find((text) => text.match(/(體育).*(選修)*/));
-      if (!result.PErequire) {
-        result.PErequire = require.groupCondition.find((text) => text.match(/(體育).*(選修)*/));
-      }
+      // result.groupCondition = require.groupCondition;
+      require.groupCondition.find((text, idx) => {
+        if (text === '群修條件說明:') {
+          result.groups = require.groupCondition.slice(idx + 1).filter((t) => t !== '無');
+          return true;
+        }
+        return false;
+      });
+      result.groupLength = result.groups.length;
 
-      result.NDrequire = require.spacialty.find((text) => text.match(/國防|軍訓/));
-      if (!result.NDrequire) {
-        result.NDrequire = require.groupCondition.find((text) => text.match(/國防|軍訓/));
+      result.gl2 = require.rules.filter((rule) => rule.group.length > 0).length;
+      if (result.gl2 > result.groupLength) {
+        // result.max = 'gl2';
+        // result.ruleGroup = require.rules.filter((rule) => rule.group.length > 0);
+        if (result.groupLength === 0) {
+          result.year = require.year;
+          result.departmentName = require.departmentName;
+          result.groupName = require.groupName;
+          // fs.appendFileSync('./data/output.json.local', `${JSON.stringify(result, null, 2)}\n`);
+          fs.appendFileSync('./data/output.json.local', `${result.year}-${result.departmentName}\n`);
+        }
       }
+      if (result.gl2 < result.groupLength) {
+        result.max = 'groupLength';
+      }
+      if (result.gl2 === result.groupLength) {
+        result.max = 'equal';
+      }
+      // result.eq = result.groupLength === result.gl2;
 
-      if (!result.PErequire || !result.NDrequire) {
-        // result.year = require.year;
-        // result.departmentName = require.departmentName;
-        // result.groupName = require.groupName;
-      } else {
-        result.PErequire = result.PErequire
-          .replace(/^\(?[0-9一二三四五六七八九十][).、]|^※|^\(|\)$|。$/, '')
-          .replace('全民國防教育軍事訓練', '軍訓');
-        result.NDrequire = result.NDrequire
-          .replace(/^\(?[0-9一二三四五六七八九十][).、]|^※|^\(|\)$|。$/, '')
-          .replace('全民國防教育軍事訓練', '軍訓');
-        // result.PEequalsND = result.PErequire === result.NDrequire;
-        fs.appendFileSync('./data/output.csv.local', `"${result.PErequire}"\n"${result.NDrequire}"\n`);
-      }
+      // console.log(require.groupCondition);
+      // result.minTotalCredit1 = require.groupCondition
+      //   .find((text) => text.match(/本[系]*[學程]*[取得學位]*最低[畢業總]*學分數：/))
+      //   .replace(/.*本[系]*[學程]*[取得學位]*最低[畢業總]*學分數：(\d+)學分.*/, '$1');
+      // result.minTotalCredit2 = [require.groupCondition[0]]
+      //   .find((text) => text.match(/本[系]*[學程]*[取得學位]*最低[畢業總]*學分數：/))
+      //   .replace(/.*本[系]*[學程]*[取得學位]*最低[畢業總]*學分數：(\d+)學分.*/, '$1');
+      // result.equal = result.minTotalCredit1 === result.minTotalCredit2;
+
+      // result.PErequire = require.spacialty.find((text) => text.match(/(體育)(?!必修).*(選修)*/));
+      // if (!result.PErequire) {
+      //   result.PErequire = require.groupCondition.find((text) => text.match(/(體育)(?!必修).*(選修)*/));
+      // }
+
+      // result.NDrequire = require.spacialty.find((text) => text.match(/國防|軍訓/));
+      // if (!result.NDrequire) {
+      //   result.NDrequire = require.groupCondition.find((text) => text.match(/國防|軍訓/));
+      // }
+
+      // if (!result.PErequire || !result.NDrequire) {
+      //   // result.year = require.year;
+      //   // result.departmentName = require.departmentName;
+      //   // result.groupName = require.groupName;
+      // } else {
+      //   result.PErequire = result.PErequire
+      //     .replaceAll(/^\(?[0-9一二三四五六七八九十][).、]|^※|^\(|\)$|。$/g, '')
+      //     .replaceAll('全民國防教育軍事訓練', '軍訓');
+      //   result.NDrequire = result.NDrequire
+      //     .replaceAll(/^\(?[0-9一二三四五六七八九十][).、]|^※|^\(|\)$|。$/g, '')
+      //     .replaceAll('全民國防教育軍事訓練', '軍訓');
+      // }
 
       // fs.appendFileSync('./data/output.json.local', `${JSON.stringify(result, null, 2)}\n`);
       return require;
